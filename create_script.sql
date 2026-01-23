d6442106da41ec1d933bb4c55d55f036c3125dee
Pavel <asinskijp188@gmail.com>
2026-01-23 15:39:56 +0300

    создан скрипт создания БД, заполнены 3 роли: ADMIN, STOREKEEPER, CASHIER

---
+++ create_script.sql
@@ -0,0 +1,22 @@
+
+
+-- ### DATABASE "grocery store inventory control system"
+
+-- ### DROP TABLES
+
+drop table if exists "auth_data";
+drop table if exists "user";
+drop table if exists "provider";
+drop table if exists "role";
+drop table if exists "order";
+drop table if exists "order_product";
+drop table if exists "product";
+drop table if exists "shalf";
+drop table if exists "sale_product";
+drop table if exists "sale";
+
+
+-- ### CREATE TABLES
+
+
+
